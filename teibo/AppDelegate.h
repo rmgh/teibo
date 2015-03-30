76c98bc14f478002e9a3fe74797a6ba4ae344aa1
rm <minowaryohei@minowa-no-MacBook-Air.local>
2015-03-30 09:41:44 +0900

    Initial Commit

---
+++ teibo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  teibo
+//
+//  Created by minowaryohei on 2015/03/30.
+//  Copyright (c) 2015年 rm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ teibo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  teibo
+//
+//  Created by minowaryohei on 2015/03/30.
+//  Copyright (c) 2015年 rm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
